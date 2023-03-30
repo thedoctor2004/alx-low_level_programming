2e821ed95eae7a3fd054160c9f507aebb6a7ddd0
imran nassih <yl8775128@gmail.com>
2023-03-30 13:51:01 -0700

    10x_alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+ *
+ * Return: Always (0) success
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char j = 'a';
+
+	while (i < 10)
+	{
+		while (j <= 'z')
+		{
+		_putchar (j);
+		j++;
+		}
+	j = 'a';
+	i++;
+	_putchar ('\n');
+	}
+}
