0d873a62b6ee682dbf013a7f7c99f101e24c0f50
imran nassih <yl8775128@gmail.com>
2023-03-30 10:20:30 -0700

    all of them

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,79 +1,23 @@
-#include "main.h"
 #include <stdio.h>
 
-/**
- * rev_array - reverse array
- * @n: integer params
- * Return: 0
- */
-
-void rev_array(char *n)
+int main(void)
 {
-	int i = 0;
-	int j = 0;
-	char temp;
+  int n;
+  int a[5];
+  int *p;
 
-	while (*(n + i) != '\0')
-	{
-		i++;
-	}
-	i--;
-
-	for (j = 0; j < i; j++, i--)
-	{
-		temp = *(n + j);
-		*(n + j) = *(n + i);
-		*(n + i) = temp;
-	}
-}
-
-/**
- * infinite_add - add 2 numbers together
- * @n1: text representation of 1st number to add
- * @n2: text representation of 2nd number to add
- * @r: pointer to buffer
- * @size_r: buffer size
- * Return: pointer to calling function
- */
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-	int carry = 0, i = 0, j = 0, digits = 0;
-	int val1 = 0, val2 = 0, total = 0;
-
-	while (*(n1 + i) != '\0')
-		i++;
-	while (*(n2 + j) != '\0')
-		j++;
-	i--;
-	j--;
-	if (j >= size_r || i >= size_r)
-		return (0);
-	while (j >= 0 || i >= 0 || carry == 1)
-	{
-		if (i < 0)
-			val1 = 0;
-		else
-			val1 = *(n1 + i) - '0';
-		if (j < 0)
-			val2 = 0;
-		else
-			val2 = *(n2 + j) - '0';
-		total = val1 + val2 + carry;
-		if (total >= 10)
-			carry = 1;
-		else
-			carry = 0;
-		if (digits >= (size_r - 1))
-			return (0);
-		r[digits] = (total % 10) + '0';
-		digits++;
-		j--;
-		i--;
-	}
-	if (digits == size_r)
-		return (0);
-	r[digits] = '\0';
-	rev_array(r);
-	return (r);
+  a[2] = 1024;
+  p = &n;
+  /*
+   * write your line of code here...
+   * Remember:
+   * - you are not allowed to use a
+   * - you are not allowed to modify p
+   * - only one statement
+   * - you are not allowed to code anything else than this line of code
+   */
+  *(p + 5) = 98;
+  /* ...so that this prints 98\n */
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
