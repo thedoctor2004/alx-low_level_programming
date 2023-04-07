70129bc6004b7bb00a7118411e06000a5b4667f1
imran nassih <yl8775128@gmail.com>
2023-04-07 08:34:58 -0700

    c - argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * main - program that prints its name, followed by a new line
+ * main - program that prints its name, followed by a new line
  * @argc: counter
  * @argv: vector
  * Return: 0
