75a1929690d51a311c8f0e44369b74213a0855fd
imran nassih <yl8775128@gmail.com>
2023-04-17 19:23:24 -0700

    C - Structures, typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,4 +14,13 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - new type dog_t as a new name for the type struct dog
+*/
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
 #endif /* DOG_H */
